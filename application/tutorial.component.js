a6aa48b74bc5c48bf235b04b3bca62d235a9274f
nttoan <nttoan@nttoan-pc>
2017-01-06 16:47:02 +0700

    lesson 12 attribute directive commit

--- application/tutorial.component.js
+++ application/tutorial.component.js
@@ -9,20 +9,32 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require('@angular/core');
+/*
+ * ngClass duoc dung de ap dung class cho 1 element dua vao gia tri cua bien
+ * cu phap [ngClass]="{ten_class_1:ten_bien_1,ten_class_2:ten_bien_2,...}"
+ *
+ * ngStyle duoc ap dung de dua thuoc tinh style vao 1 element dua vao gia tri cua bien, nham thay doi cach the hien
+ *
+ * cu phap [ngStyle]="{'font-style: style, font-size: size'}" voi style va size la 2 bien
+ *
+*/
 var TutorialComponent = (function () {
     function TutorialComponent() {
         this.title = "This is Angular 2 TutorialComponent";
-        this.showLineIf = false;
-        this.color = 'blue';
-        this.colors = ['red', 'green', 'blue'];
+        this.cone = false;
+        this.ctwo = true;
     }
-    TutorialComponent.prototype.OnClick = function (value) {
-        console.log(value);
+    TutorialComponent.prototype.toggle = function () {
+        this.cone = !this.cone;
+        this.ctwo = !this.ctwo;
     };
     TutorialComponent = __decorate([
         core_1.Component({
             selector: "my-tutorial",
-            template: "\n    <h2>{{title}}</h2> \n    <h3 *ngIf=\"showLineIf\">This is ngIf directive line.</h3>\n    <div [ngSwitch]=\"color\">\n        <p *ngSwitchCase=\"'red'\">This line is red</p>\n        <p *ngSwitchCase=\"'blue'\">This line is blue</p>\n        <p *ngSwitchCase=\"'green'\">This line is green</p>\n        <p *ngSwitchDefault>Invalid color</p>\n\n\n    </div>\n    <ul>\n        <li *ngFor=\"let c of colors\">This is color {{c}}</li>\n    </ul>\n    "
+            template: "\n    <h2>{{title}}</h2>    \n    <p [ngClass]=\"{classOne:cone, classTwo:ctwo}\">This is a result of ngClass</p>\n    <button (click)=\"toggle()\">Change color</button>\n   \n    ",
+            styles: [
+                "\n            .classOne{\n                color:red\n            }\n            \n            .classTwo{\n                color:blue\n            }\n        "
+            ]
         }), 
         __metadata('design:paramtypes', [])
     ], TutorialComponent);
--- application/tutorial.component.ts
+++ application/tutorial.component.ts
@@ -1,30 +1,40 @@
 import { Component } from '@angular/core';
-
+/*
+ * ngClass duoc dung de ap dung class cho 1 element dua vao gia tri cua bien
+ * cu phap [ngClass]="{ten_class_1:ten_bien_1,ten_class_2:ten_bien_2,...}"
+ * 
+ * ngStyle duoc ap dung de dua thuoc tinh style vao 1 element dua vao gia tri cua bien, nham thay doi cach the hien
+ * 
+ * cu phap [ngStyle]="{'font-style: style, font-size: size'}" voi style va size la 2 bien
+ * 
+*/
 @Component({
     selector: "my-tutorial",
     template: `
-    <h2>{{title}}</h2> 
-    <h3 *ngIf="showLineIf">This is ngIf directive line.</h3>
-    <div [ngSwitch]="color">
-        <p *ngSwitchCase="'red'">This line is red</p>
-        <p *ngSwitchCase="'blue'">This line is blue</p>
-        <p *ngSwitchCase="'green'">This line is green</p>
-        <p *ngSwitchDefault>Invalid color</p>
-
-
-    </div>
-    <ul>
-        <li *ngFor="let c of colors">This is color {{c}}</li>
-    </ul>
-    `
+    <h2>{{title}}</h2>    
+    <p [ngClass]="{classOne:cone, classTwo:ctwo}">This is a result of ngClass</p>
+    <button (click)="toggle()">Change color</button>
+   
+    `,
+    styles: [
+        `
+            .classOne{
+                color:red
+            }
+            
+            .classTwo{
+                color:blue
+            }
+        `
+    ]
 })
 
 export class TutorialComponent {
     title = "This is Angular 2 TutorialComponent";
-    showLineIf = false;
-    color='blue';
-    colors:string[]=['red','green','blue'];
-    OnClick(value: string) {
-        console.log(value);
+    cone = false;
+    ctwo = true;
+    toggle() {
+        this.cone = !this.cone;
+        this.ctwo = !this.ctwo;
     }
 }
